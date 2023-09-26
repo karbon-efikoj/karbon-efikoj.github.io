1a0ab94786413d20dd62ef5af957d77ded2de8cb
Wolfram Diestel <wolfram.diestel@t-systems.com>
2023-09-26 08:37:20 +0200

    +metaneligo

---
+++ _temoj/metano.md
@@ -0,0 +1,10 @@
+---
+layout: page
+title: Elgasiĝo de metano
+temo: metano
+fontoj:
+  - lng: en
+    en: "large methane emission plumes across the globe detected by TROPOMI onboard Sentinel-5P"
+    eo: "Ĉiusemajnaj mondmapoj de metaneligo detektitaj per instrumento TROPOMI de la satelito Sentinel-5P"
+    url: https://earth.sron.nl/methane-emissions/
+---
--- prov-servado.sh
+++ prov-servado.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+echo "Tiu skripto servas la retpaĝojn loke uzante jekyll - simile kiel tio okazas en la publika paĝo"
+echo "Ĉiu konservita paĝo tiam tuj konvertiĝas kaj estas spektebla en retumilo ĉe http://0.0.0.0:4000"
+echo ""
+echo "Unue necesas instali pakaĵoj laŭ la enhavo en Gemfile kaj antaŭe certigu aktualigitan"
+echo "sistemon:"
+echo "# sudo gem update --system"
+echo "# bundle install"
+
 bundle exec jekyll serve --draft --trace
